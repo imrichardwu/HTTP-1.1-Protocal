9493ce1fd4623d5626a2c716774a26ff9c0b32c0
imrichardwu <richard9@ualberta.ca>
2025-08-24 14:53:30 -0600

    Refactor request parsing to use chunkReader for simulating network reads and improve error handling

--- internal/request.go
+++ internal/request.go
@@ -1,10 +1,9 @@
 package request
 
 import (
-	"errors"
+	"bytes"
 	"fmt"
 	"io"
-	"strings"
 )
 
 type RequestLine struct {
@@ -19,55 +18,111 @@ func (r *RequestLine) ValidHTTP() bool {
 
 type Request struct {
 	RequestLine RequestLine
+	state       parserState
 }
 
-var SEPERATOR = "\r\n"
-var MELFORM_REQUEST_LINE = fmt.Errorf("melform request-line")
-var ERROR_UNSUPPORTED_HTTP_VERSION = fmt.Errorf("unsupported http version")
+var SEPERATOR = []byte("\r\n")
+var ErrorMalformedRequestLine = fmt.Errorf("melform request-line")
+var ErrorUnsportedHttpVersion = fmt.Errorf("unsupported http version")
+var ErrorRequestInErrorState = fmt.Errorf("request in error state")
 
-func parseRequestLine(b string) (*RequestLine, string, error) {
-	idx := strings.Index(b, SEPERATOR)
+type parserState string
+
+const (
+	StateInit  parserState = "init"
+	StateDone  parserState = "done"
+	StateError parserState = "error"
+)
+
+func newRequest() *Request {
+	return &Request{
+		state: StateInit,
+	}
+}
+
+func parseRequestLine(b []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(b, SEPERATOR)
 	if idx == -1 {
-		return nil, b, nil
+		return nil, 0, nil
 	}
 	startLine := b[:idx]
-	restOfMfg := b[idx+len(SEPERATOR):]
+	read := idx + len(SEPERATOR)
 
-	parts := strings.Split(startLine, " ")
+	parts := bytes.Split(startLine, []byte(" "))
 
 	// If we do not have method, path, and HTTP protocal
 	if len(parts) != 3 {
-		return nil, restOfMfg, MELFORM_REQUEST_LINE
+		return nil, 0, ErrorMalformedRequestLine
 	}
 
-	httpParts := strings.Split(parts[2], "/")
+	httpParts := bytes.Split(parts[2], []byte("/"))
 
-	if len(httpParts) != 2 || httpParts[0] != "HTTP" || httpParts[1] != "1.1" {
-		return nil, restOfMfg, MELFORM_REQUEST_LINE
+	if len(httpParts) != 2 || string(httpParts[0]) != "HTTP" || string(httpParts[1]) != "1.1" {
+		return nil, 0, ErrorMalformedRequestLine
 	}
 
 	rl := &RequestLine{
-		Method:        parts[0],
-		RequestTarget: parts[1],
-		HttpVersion:   httpParts[1],
+		Method:        string(parts[0]),
+		RequestTarget: string(parts[1]),
+		HttpVersion:   string(httpParts[1]),
 	}
 
-	return rl, restOfMfg, nil
+	return rl, read, nil
 
 }
 
+func (r *Request) parse(data []byte) (int, error) {
+	read := 0
+outer:
+	for {
+
+		switch r.state {
+		case StateInit:
+			rl, n, err := parseRequestLine(data)
+			if err != nil {
+				r.state = StateError
+				return 0, ErrorRequestInErrorState
+			}
+
+			if n == 0 {
+				break outer
+			}
+
+			r.RequestLine = *rl
+			read += n
+
+			r.state = StateDone
+		case StateDone:
+			break outer
+		}
+	}
+	return read, nil
+}
+
+func (r *Request) done() bool {
+	return r.state == StateDone || r.state == StateError
+}
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, errors.Join(fmt.Errorf("unable to io.ReadALL: %w", err))
+	request := newRequest()
+
+	buf := make([]byte, 1024)
+	bufLen := 0
+	for !request.done() {
+		n, err := reader.Read(buf[bufLen:])
+		if err != nil {
+			return nil, err
+		}
+
+		bufLen += n
+		readN, err := request.parse(buf[:bufLen+n])
+		if err != nil {
+			return nil, err
+		}
+
+		copy(buf, buf[readN:bufLen])
+		bufLen -= readN
 	}
 
-	str := string(data)
-	rl, _, err := parseRequestLine(str)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &Request{RequestLine: *rl}, err
+	return request, nil
 }
